cf8e7d84d37dfafbf80e4d98cd62cdd246f34d00
JTK76873 <JTK76873@creighton.edu>
2020-02-03 14:56:17 -0600

    Update WelcomeForm.js

--- forms/WelcomeForm/WelcomeForm.js
+++ forms/WelcomeForm/WelcomeForm.js
@@ -2,5 +2,6 @@ let names = ["Joe", "Sally"]
 
 btnShowMessage.onclick=function(){
   console.log(`My best friend is ${names[0]}`)
-  labMessage.textContent = "I really enjoy" + names[0]
+alert(`The first person in the array is ${names[0]}`)
+labMessage.textContent = "I really enjoy" + names[0]
 }
